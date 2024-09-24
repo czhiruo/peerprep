eeeba3d9777eb0f2b68d897f072828d71d86647d
peiran18 <e0968798@u.nus.edu>
2024-09-24 15:54:46 +0800

    Added Update and Delete

--- backend/question-service/controllers/controllers.ts
+++ backend/question-service/controllers/controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { querySchema, createSchema } from "../validations/querySchema.js";
+import { querySchema, createSchema, updateSchema} from "../validations/querySchema.js";
 
 class QuestionController {
   async get(req: Request, res: Response) {
@@ -33,11 +33,33 @@ class QuestionController {
   }
 
   async update(req: Request, res: Response) {
-    // ...
+    const validation = updateSchema.safeParse(req.body);
+
+    if (!validation.success) {
+      return res.status(400).json({ errors: validation.error.format() });
+    }
+
+    const { id, title, desc, c, d } = validation.data;
+
+    res.status(200).json(
+      `Question with id ${id} updated with: 
+        title: ${title}, 
+        desc: ${desc}, 
+        category: ${c}, 
+        difficulty: ${d}`
+    );
   }
 
   async delete(req: Request, res: Response) {
-    // ...
+    const validation = querySchema.safeParse(req.params); 
+
+    if (!validation.success) {
+      return res.status(400).json({ errors: validation.error.format() });
+    }
+
+    const { id } = validation.data;
+
+    res.status(200).json(`Question with id ${id} has been deleted.`);
   }
 }
 
