0068fb902caa266dab4dc0fd4b23a0d5018da21b
zhiruo <e0967827@u.nus.edu>
2024-09-27 22:03:45 +0800

    updated the reference to json data in frontend in QuestionList

--- frontend/question-repository/src/components/QuestionList.js
+++ frontend/question-repository/src/components/QuestionList.js
@@ -33,18 +33,22 @@ const QuestionList = () => {
       <h2>Question Repository</h2>
       <Link to="/add">Add New Question</Link>
       <ul>
-        {questions.map((question) => (
-          <li key={question.id}>
-            <h3>{question.title}</h3>
-            <p>{question.description}</p>
+        {questions.map((item) => (
+          <li key={item._id}>
+            <h3>{item.question.questionTitle}</h3>
+            <p>{item.question.questionDescription}</p>
             <p>
-              <strong>Category:</strong> {question.category}
+              <strong>Category:</strong>
+              {/* Checks if question belongs to more than one category */}
+              {Array.isArray(item.question.questionCategory)
+                ? item.question.questionCategory.join(", ")
+                : item.question.questionCategory}
             </p>
             <p>
-              <strong>Complexity:</strong> {question.complexity}
+              <strong>Complexity:</strong> {item.question.questionComplexity}
             </p>
-            <Link to={`/edit/${question.id}`}>Edit</Link>
-            <button onClick={() => deleteQuestion(question.id)}>Delete</button>
+            <Link to={`/edit/${item._id}`}>Edit</Link>
+            <button onClick={() => deleteQuestion(item._id)}>Delete</button>
           </li>
         ))}
       </ul>
