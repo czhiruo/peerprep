14864096b219e4acfb2e16d6a32f34aef88b1d5b
Martin <martin.ng@live.com>
2024-09-28 19:08:23 +0800

    Change PUT to PATCH for updating questions

--- backend/question-service/src/routes/routes.ts
+++ backend/question-service/src/routes/routes.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/", questionController.get); // Get all questions or filter
 router.get("/:id", questionController.getById); // Get question by id
 router.post("/", questionController.create); // Create a new question
-router.put("/:id", questionController.update); // Update a question
+router.patch("/:id", questionController.update); // Update a question
 router.delete("/:id", questionController.delete); // Delete a question
 
 export default router;
