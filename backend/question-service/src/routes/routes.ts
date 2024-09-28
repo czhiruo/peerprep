c380cff3aad310cb9cdc585d46ff04cf97c319ff
Martin <martin.ng@live.com>
2024-09-28 18:36:49 +0800

    Update API endpoints

--- backend/question-service/src/routes/routes.ts
+++ backend/question-service/src/routes/routes.ts
@@ -3,9 +3,10 @@ import { questionController } from "../controllers/controllers.js";
 
 const router = Router();
 
-router.get("/", questionController.get);
-router.post("/", questionController.create);
-router.put("/", questionController.update);
-router.delete("/", questionController.delete);
+router.get("/", questionController.get); // Get all questions or filter
+router.get("/:id", questionController.getById); // Get question by id
+router.post("/", questionController.create); // Create a new question
+router.put("/:id", questionController.update); // Update a question
+router.delete("/:id", questionController.delete); // Delete a question
 
 export default router;
