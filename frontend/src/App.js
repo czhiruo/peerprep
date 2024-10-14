780ab8410e6da3c75200e63a803006e70a2a1f29
peiran18 <e0968798@u.nus.edu>
2024-10-14 21:23:06 +0800

    Added Matching Page ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import SignUpPage from './pages/SignUpPage';
 import SelectComplexityPage from './pages/SelectComplexityPage';
 import SelectLanguagePage from './pages/SelectLanguagePage';
 import SelectTopicPage from './pages/SelectTopicPage';
+import MatchingPage from './pages/MatchingPage';
 import PrivateRoute from './components/PrivateRoute';
 import AdminRoute from './components/AdminRoute';
 import Header from './components/Header';
@@ -22,10 +23,11 @@ function App() {
       {/* Main Content */}
       <main className="flex-grow">
         <Routes>
-          <Route path="/complexity" element={<PrivateRoute><SelectComplexityPage /></PrivateRoute>} />
           <Route path="/topic" element={<PrivateRoute><SelectTopicPage /></PrivateRoute>} />
+          <Route path="/complexity" element={<PrivateRoute><SelectComplexityPage /></PrivateRoute>} />
           <Route path="/language" element={<PrivateRoute><SelectLanguagePage /></PrivateRoute>} />
-          <Route path="/" element={<SelectLanguagePage />} />
+          <Route path="/language" element={<PrivateRoute><MatchingPage /></PrivateRoute>} />
+          <Route path="/" element={<MatchingPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/reset" element={<ResetPasswordPage />} />
